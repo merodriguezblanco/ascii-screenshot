cf24e6274e6b512376c674675b8b1a9abf0bc3ac
Martin Rodriguez <merodriguezblanco@gmail.com>
2015-03-29 13:49:29 -0300

    Convert screenshot to color ASCII.

--- public/script.js
+++ public/script.js
@@ -57,10 +57,62 @@
     return asciiScreenshot;
   }
 
+  var createColorPixel = function (pixel, pixelColor) {
+    var colorPixel = '<span style="color:rgb(' + pixelColor.red + ',' + pixelColor.green + ',' + pixelColor.blue + ')">' + pixel + '</span>';
+    return colorPixel;
+  }
+
+  var convertScreenshotToColorASCII = function () {
+    // The canvas image data will behave as the Bitmap for the screenshot.
+    var ASCII_CHARS = '#@%OHLTI)i=+;:,. '.split(''),
+        ASCII_CHARS_LENGTH = ASCII_CHARS.length - 1,
+        CANVAS_WIDTH = canvas.width,
+        CANVAS_HEIGHT = canvas.height,
+        context = canvas.getContext('2d'),
+        imageData = context.getImageData(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT).data,
+        asciiScreenshot = '',
+        pixelColor = {},
+        x, y, offset, red, blue, green, pixel;
+
+    // Loop through each pixel of the screenshot.
+    for (y = 0; y < CANVAS_HEIGHT; y += 1) {
+      for (x = 0; x < CANVAS_WIDTH; x += 1) {
+        offset = (y * CANVAS_WIDTH + x) * 4;
+
+        // Get grayscale color from screenshot.
+        // The easiest way is to get the biggest of the RGB values
+        // of each pixel and divide by 255.
+        red = imageData[offset];
+        green = imageData[offset + 1];
+        blue = imageData[offset + 2];
+        pixel = Math.max(red, green, blue) / 255;
+
+        pixelColor = {
+          red: red,
+          green: green,
+          blue: blue
+        }
+
+        // Select ASCII that corresponds to grayscale and append.
+        asciiScreenshot += createColorPixel(ASCII_CHARS[parseInt(pixel * ASCII_CHARS_LENGTH, 10)], pixelColor);
+      }
+
+      asciiScreenshot += '\n';
+    }
+
+    return asciiScreenshot;
+  }
+
   var drawGrayscaleScreenshot = function () {
     var grayContainer = document.querySelector('#grayscale-screenshot');
 
     grayContainer.innerHTML = convertScreenshotToGrayscaleASCII();
+  }
+
+  var drawColorScreenshot = function () {
+    var colorContainer = document.querySelector('#color-screenshot');
+
+    colorContainer.innerHTML = convertScreenshotToColorASCII();
   }
 
   var drawScreenshotOnCanvas = function () {
@@ -77,6 +129,9 @@
 
     // Convert screenshot to grayscale and display it.
     drawGrayscaleScreenshot();
+
+    // Convert screenshot to color and display it.
+    drawColorScreenshot();
   }
 
   // Start getting media from camera.
